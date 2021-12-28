13d679455d092e09a3da09e9df5de362c210faf2
Lee Penkman <leepenkman@gmail.com>
2021-12-29 08:04:20 +1100

    add cron

--- predict_stock_e2e.py
+++ predict_stock_e2e.py
@@ -5,7 +5,7 @@ from data_curate import download_daily_stock_data
 # from predict_stock import make_predictions
 from decorator_utils import timeit
 from predict_stock_forecasting import make_predictions
-import pandas as pd
+
 
 @timeit
 def do_forecasting():
@@ -39,8 +39,6 @@ def buy_stock(row):
         alpaca_wrapper.buy_stock(currentBuySymbol, row)
 
 
-
-
 def make_trade_suggestions(predictions):
     # sort df by close predicted price
     # where closemin_loss_trading_profit is positive
@@ -56,9 +54,7 @@ def make_trade_suggestions(predictions):
             break
 
 
-
 if __name__ == '__main__':
     # in development, use the following line to avoid re downloading data
     do_forecasting()
     # make_trade_suggestions(pd.read_csv('/home/lee/code/stock/results/predictions-2021-12-23_23-04-07.csv'))
-
---
+++ scripts/run_trading_bot.sh
@@ -0,0 +1,5 @@
+
+
+cd ~/code/stock
+. .env/bin/activate
+python /home/lee/code/stock/predict_stock_e2e.py
