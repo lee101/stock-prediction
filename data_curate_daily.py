363935180b37b756ee1ca771da82074d9980aed9
Lee Penkman <leepenkman@gmail.com>
2022-01-29 19:41:30 +1300

    fix crypto download of data

--- data_curate_daily.py
+++ data_curate_daily.py
@@ -85,9 +85,12 @@ def download_daily_stock_data(path=None):
         # start = pd.Timestamp('2020-08-28 9:30', tz=NY).isoformat()
         # end = pd.Timestamp('2020-08-28 16:00', tz=NY).isoformat()
         ## print(api.get_barset(['AAPL', 'GOOG'], 'minute', start=start, end=end).df)
-
-        minute_df = api.get_bars(symbol, TimeFrame(1, TimeFrameUnit.Day), start, end,
-                                 adjustment='raw').df
+        if symbol in ['BTCUSD', 'ETHUSD', 'LTCUSD']:
+            minute_df = api.get_crypto_bars(symbol, TimeFrame(1, TimeFrameUnit.Day), start, end,
+                                     adjustment='raw').df
+        else:
+            minute_df = api.get_bars(symbol, TimeFrame(1, TimeFrameUnit.Day), start, end,
+                                     adjustment='raw').df
         if minute_df.empty:
             print(f"{symbol} has no data")
             continue
