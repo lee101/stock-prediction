c0966048ab76d2afdc3fb08c91ce395c57a56e3a
Lee Penkman <leepenkman@gmail.com>
2022-02-06 00:47:06 +1300

    crypto fix

--- alpaca_wrapper.py
+++ alpaca_wrapper.py
@@ -168,10 +168,10 @@ def buy_stock(currentBuySymbol, row, price, margin_multiplier=1.95, side='long')
     try:
         current_price = row['close_last_price_minute']
         amount_to_trade = int(notional_value / current_price)
-        if amount_to_trade < 1: # TODO fix for fractional/crypto which under 1 is fine
             if currentBuySymbol in ["BTCUSD", "ETHUSD", "LTCUSD"]:
-                amount_to_trade = .001
-            else:
+                if amount_to_trade < .001:
+                    amount_to_trade = .001
+            elif amount_to_trade < 1:
                 amount_to_trade = 1
 
         if side == 'short':
