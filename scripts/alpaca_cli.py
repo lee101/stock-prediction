366eeed178c7d9932771e21cb92a5ec8081eb2d0
Lee Penkman <leepenkman@gmail.com>
2024-07-04 19:34:56 +1200

    more aggressive sell ramp to exit positions - todo market order as a last resort

--- scripts/alpaca_cli.py
+++ scripts/alpaca_cli.py
@@ -73,14 +73,13 @@ def backout_near_market(pair, start_time=None):
         found_position = False
         for position in positions:
             if position.symbol == pair:
-                pct_above_market = 0.026
+                pct_above_market = 0.02
                 linear_ramp  = 60
                 minutes_since_start = (datetime.now() - start_time).seconds // 60
                 if minutes_since_start >= linear_ramp:
-                    pct_above_market = 0.00
+                    pct_above_market = -0.02
                 else:
-                    pct_above_market = pct_above_market - (pct_above_market * minutes_since_start / linear_ramp)
-                pct_above_market -= .01 # from .016 to -.006 to ensure orders close
+                    pct_above_market = pct_above_market - (0.04 * minutes_since_start / linear_ramp)
 
                 logger.info(f"pct_above_market: {pct_above_market}")
                 succeeded = alpaca_wrapper.close_position_near_market(position, pct_above_market=pct_above_market)
