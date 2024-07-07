171e20467cc127780d5447b0522f25503dac9b13
Lee Penkman <leepenkman@gmail.com>
2024-07-08 10:25:24 +1200

    fixlog

--- src/process_utils.py
+++ src/process_utils.py
@@ -12,6 +12,7 @@ def backout_near_market(symbol):
     subprocess.Popen(
         command,
         shell=True,
-        # stdout=subprocess.DEVNULL,
-        # stderr=subprocess.DEVNULL
-        )
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+        start_new_session=True
+    )
--- tests/integ/test_process_utils.py
+++ tests/integ/test_process_utils.py
@@ -3,3 +3,4 @@ from src.process_utils import backout_near_market
 
 def test_backout_near_market():
     backout_near_market("BTCUSD")
+    print('done')
